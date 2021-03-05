0a51d0661fabba0917a21974a60951b1a4c4284f
Pablo Samuel Castro <psc@google.com>
2021-03-05 12:53:16 -0500

    Update agents_mult_runs_all.py

--- Tests_Johan/xavier_vs_scalevalues/agents_mult_runs_all.py
+++ Tests_Johan/xavier_vs_scalevalues/agents_mult_runs_all.py
@@ -4,50 +4,40 @@ from dopamine.discrete_domains import run_experiment
 import gin.tf
 
 import sys
-from dqn_agent_new import*
-from rainbow_agent_new import*
-from quantile_agent_new import*
-from implicit_quantile_agent_new import*
+from dqn_agent_new import *
+from rainbow_agent_new import *
+from quantile_agent_new import *
+from implicit_quantile_agent_new import *
 
 
-#.gin files-> xavier or variance_scalin
-inf = {1:{'gin':'dqn_acrobot.gin',
-          'path': 'DQN'},
-      2:{'gin':'rainbow_acrobot.gin',
-          'path': 'Rainbow'},
-      3: {'gin':'quantile_acrobot.gin',
-          'path': 'QR'},
-      4:{'gin':'implicit_acrobot.gin',
-          'path': 'IQ'}
-          }
+agents = {
+    'dqn': JaxDQNAgentNew,
+    'rainbow': JaxRainbowAgentNew,
+    'quantile': JaxQuantileAgentNew,
+    'implicit': JaxImplicitQuantileAgentNew,
+}
 
-for agnt in range (1,5):
-  path = inf[agnt]['path']
+num_runs = 7
 
-  for i in range (1,8):
+environments = ['cartpole', 'acrobot']
 
-    LOG_PATH = os.path.join(path, 'dqn_test'+str(i))
-    sys.path.append(path)
+for agent in agents:
+  for env in environments:
+    for i in range (1, num_runs + 1):
 
-    if agnt == 1:
-        def create_random_dqn_agent(sess, environment, summary_writer=None):
-           return JaxDQNAgentNew(num_actions=environment.action_space.n)
-    elif agnt == 2:
-    	def create_random_dqn_agent(sess, environment, summary_writer=None):
-    		return JaxRainbowAgentNew(num_actions=environment.action_space.n)
-    elif agnt == 3:
-    	def create_random_dqn_agent(sess, environment, summary_writer=None):
-    	  	return JaxQuantileAgentNew(num_actions=environment.action_space.n)
-    elif agnt == 4:
-    	def create_random_dqn_agent(sess, environment, summary_writer=None):
-    		return JaxImplicitQuantileAgentNew(num_actions=environment.action_space.n)
-    else:
-    	print('error!')
+      LOG_PATH = os.path.join(agent, f'dqn_test{i}')
+      sys.path.append(path)
+          
+      def create_agent(sess, environment, summary_writer=None):
+        return agents[agent](num_actions=environment.action_space.n)
 
-    gin.parse_config_file(inf[agnt]['gin'])
+      gin_file = f'{agent}_{env}.gin'
+      gin.parse_config_file(gin_file)
+      # gin_bindings = ["JaxDQNAgentNew.initzer='variance_scaling_5'", ...]
+      # gin.parse_config_files_and_bindings([gin_file], gin_bindings, skip_unknown=False)
 
-    random_dqn_runner = run_experiment.TrainRunner(LOG_PATH, create_random_dqn_agent)
+      agent_runner = run_experiment.TrainRunner(LOG_PATH, create_agent)
 
-    print('Will train agent, please be patient, may be a while...')
-    random_dqn_runner.run_experiment()
-    print('Done training!')
+      print(f'Will train agent {agent} in {env}, run {i}, please be patient, may be a while...')
+      agent_runner.run_experiment()
+      print('Done training!')
