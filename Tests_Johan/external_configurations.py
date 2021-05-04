3c513d4d468a947f1750e29da100a38fa3eff745
johan <jobando0730@gmail.com>
2021-05-04 15:54:48 -0500

    code initializers

---
+++ Tests_Johan/external_configurations.py
@@ -0,0 +1,14 @@
+"""External configuration .gin"""
+
+from gin import config
+#import jax
+from flax import linen as nn
+
+
+#config.external_configurable(jax.nn.initializers.zeros, 'jax.nn.initializers.zeros')
+#config.external_configurable(jax.nn.initializers.ones, 'jax.nn.initializers.ones')
+#config.external_configurable(jax.nn.initializers.variance_scaling, 'jax.nn.initializers.variance_scaling')
+
+config.external_configurable(nn.initializers.zeros, 'nn.initializers.zeros')
+config.external_configurable(nn.initializers.ones, 'nn.initializers.ones')
+config.external_configurable(nn.initializers.variance_scaling, 'nn.initializers.variance_scaling')
