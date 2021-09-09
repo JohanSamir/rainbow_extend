a52df93d838a43b3cf82c957a2fba0f746549382
johan <jobando0730@gmail.com>
2021-09-09 17:32:54 -0400

    integrating

--- epsilons/tmux-runs.py
+++ epsilons/tmux-runs.py
@@ -4,12 +4,18 @@ import itertools
 agents = ["rainbow"]
 seeds = list(range(1, 11))
 environments = ["acrobot", "cartpole", "lunarlander", "mountaincar"]
-trials = list(dict([('agent', ag), ('env', env), ('seed', seed)]) for (ag, env, seed) in itertools.product(agents, environments, seeds))
+experiments = ["epsilon"]
+weights_biases = [False]
+
+trials = list(dict([('agent', ag), ('env', env), ('seed', seed), ('experiment', exp), ('weights_biases', wb) ]) for (ag, env, seed, exp, wb) in itertools.product(agents, environments, seeds, experiments, weights_biases))
 
 for hyperparameters in trials:
     hyperparameters = dict(hyperparameters)
     env = hyperparameters["env"]
     seed = hyperparameters["seed"]
     agent = hyperparameters["agent"]
+    exp = hyperparameters["experiment"]
+    wb = hyperparameters["weights_biases"]
+
     #os.system(f'tmux new-session -d -s {env}_{agent}_{seed} sh -c "conda activate rain; python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed}"')
-    os.system(f'tmux new-session -d -s {env}_{agent}_{seed} sh -c "python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed}"')
+    os.system(f'tmux new-session -d -s {env}_{agent}_{seed}_{exp} sh -c "python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed} --experiment={exp} --weights_biases={wb}"')
