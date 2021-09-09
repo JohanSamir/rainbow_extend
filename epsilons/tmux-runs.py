ef9ca334733c0eb6561b519af6bbfa74affdb2b2
joao guilherme <joaoguilhermearujo@gmail.com>
2021-09-09 20:04:18 -0300

    Update tmux-runs.py
    removes weights and biases parameter from tmux-runs

--- epsilons/tmux-runs.py
+++ epsilons/tmux-runs.py
@@ -7,7 +7,7 @@ environments = ["acrobot", "cartpole", "lunarlander", "mountaincar"]
 experiments = ["epsilon"]
 weights_biases = [False]
 
-trials = list(dict([('agent', ag), ('env', env), ('seed', seed), ('experiment', exp), ('weights_biases', wb) ]) for (ag, env, seed, exp, wb) in itertools.product(agents, environments, seeds, experiments, weights_biases))
+trials = list(dict([('agent', ag), ('env', env), ('seed', seed), ('experiment', exp)]) for (ag, env, seed, exp, wb) in itertools.product(agents, environments, seeds, experiments))
 
 for hyperparameters in trials:
     hyperparameters = dict(hyperparameters)
@@ -15,7 +15,6 @@ for hyperparameters in trials:
     seed = hyperparameters["seed"]
     agent = hyperparameters["agent"]
     exp = hyperparameters["experiment"]
-    wb = hyperparameters["weights_biases"]
 
     #os.system(f'tmux new-session -d -s {env}_{agent}_{seed} sh -c "conda activate rain; python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed}"')
-    os.system(f'tmux new-session -d -s {env}_{agent}_{seed}_{exp} sh -c "python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed} --experiment={exp} --weights_biases={wb}"')
+    os.system(f'tmux new-session -d -s {env}_{agent}_{seed}_{exp} sh -c "python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed} --experiment={exp}"')
