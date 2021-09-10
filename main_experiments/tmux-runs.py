b51777623df6d079d352c1eb7312333acf9b3159
johan <jobando0730@gmail.com>
2021-09-10 09:32:14 -0400

    update_period added to some scripts

--- main_experiments/tmux-runs.py
+++ main_experiments/tmux-runs.py
@@ -5,7 +5,7 @@ agents = ["dqn", "rainbow"]
 types = ["online", "offline"]
 seeds = list(range(1, 11))
 environments = ["acrobot", "cartpole", "lunarlander", "mountaincar"]
-experiments = ["activation", "depth", "epsilon", "init", "learning_rate", "normalization", "width"]
+experiments = ["activation", "depth", "epsilon", "init", "learning_rate", "normalization", "width", "update_period"]
 
 trials = list(dict([('agent', ag), ('env', env), ('seed', seed), ('experiment', exp), ("type", typ)]) for (ag, env, seed, exp, typ) in itertools.product(agents, environments, seeds, experiments, types))
 
@@ -18,4 +18,4 @@ for hyperparameters in trials:
     exp = hyperparameters["experiment"]
 
     #os.system(f'tmux new-session -d -s {env}_{agent}_{seed} sh -c "conda activate rain; python3 main_online_experiments.py --env={env} --agent={agent} --initial_seed={seed}"')
-    os.system(f'tmux new-session -d -s {env}_{agent}_{seed}_{exp} sh -c "python3 main.py --env={env} --agent={agent} --initial_seed={seed} --experiment={exp} --type={typ}"')
+    os.system(f'tmux new-session -d -s {typ}_{env}_{agent}_{seed}_{exp} sh -c "python3 main.py --env={env} --agent={agent} --initial_seed={seed} --experiment={exp} --type={typ}"')
