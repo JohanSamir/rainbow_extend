7849f68bb87d9f23c352ed5d7c3be525e2c96edc
joaogui1 <joaoguilhermearujo@gmail.com>
2022-01-12 22:08:40 +0000

    2 types of seed

--- lifting_veil/xmanager_exp.py
+++ lifting_veil/xmanager_exp.py
@@ -22,7 +22,9 @@ flags.DEFINE_string("env", "cartpole", "the environment the experiment will be r
 
 flags.DEFINE_string("agent", "dqn", "the agent used in the experiment")
 
-flags.DEFINE_integer("seed", "1", "the program PRNG seed")
+flags.DEFINE_integer("rl_seed", "1", "the agent-environment PRNG seed")
+
+flags.DEFINE_integer("sample_seed", "1", "the sampling PRNG seed")
 
 flags.DEFINE_string("experiment", "effective_horizon", "the experiment will be run in")
 
@@ -40,7 +42,7 @@ def main(_):
     
     path = FLAGS.base_path
     grp = FLAGS.experiment
-    values = utils.sample_group(grp, FLAGS.seed)[0]
+    values = utils.sample_group(grp, FLAGS.sample_seed)[0]
 
     agent_name = utils.agents[FLAGS.agent].__name__
 
@@ -49,13 +51,13 @@ def main(_):
     gin.clear_config()
     gin_bindings = []
     for exp, value in zip(utils.groups[grp], values):
-        gin_bindings.extend(utils.get_gin_bindings(exp, agent_name, FLAGS.seed, value, False))
+        gin_bindings.extend(utils.get_gin_bindings(exp, agent_name, FLAGS.rl_seed, value, False))
     gin.parse_config_files_and_bindings([gin_file], gin_bindings, skip_unknown=False)
-    LOG_PATH = os.path.join(f'{path}/{FLAGS.agent}/{FLAGS.env}/{grp}_{utils.repr_values(values)}', f'test{FLAGS.seed}')
+    LOG_PATH = os.path.join(f'{path}/{FLAGS.agent}/{FLAGS.env}/{grp}_{utils.repr_values(values)}', f'test{FLAGS.rl_seed}')
     print(f"Saving data at {LOG_PATH}")
     agent_runner = run_experiment.TrainRunner(LOG_PATH, create_agent)
 
-    print(f'Training agent {FLAGS.seed}, please be patient, may be a while...')
+    print(f'Training agent {FLAGS.rl_seed}, please be patient, may be a while...')
     agent_runner.run_experiment()
     print('Done training!')
 
