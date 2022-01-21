353ecff404c4c718c472261b3e7075c5443c47f3
JohanSamir <jobando0730@gmail.com>
2022-01-20 23:50:50 -0500

    fixing minor bugs xmanager_exp.py

--- lifting_veil/xmanager_exp.py
+++ lifting_veil/xmanager_exp.py
@@ -51,9 +51,9 @@ def main(_):
     agent_name = utils.agents[FLAGS.agent].__name__
 
     if FLAGS.category == "atari_100k":
-        gin_file = f'Configs/{FLAGS.agent}.gin'
+        gin_file = f'Configs/{FLAGS.agent}_atari_100k.gin'
         gin.clear_config()
-        gin_bindings = f'create_atari_environment.game_name={FLAGS.env}'
+        gin_bindings = [f'create_atari_environment.game_name={FLAGS.env}']
     else:
         gin_file = f'Configs/{FLAGS.agent}_{FLAGS.env}.gin'
         gin.clear_config()
